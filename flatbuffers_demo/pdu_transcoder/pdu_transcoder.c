010ab74019cf7950149731b0bf5782b7831c56d6
psomadop <34111635+psomadop@users.noreply.github.com>
2017-12-30 16:38:19 +0200

    Update pdu_transcoder.c

--- flatbuffers_demo/pdu_transcoder/pdu_transcoder.c
+++ flatbuffers_demo/pdu_transcoder/pdu_transcoder.c
@@ -8,14 +8,6 @@
 
 /**
  * Encode tx_pdu, to the supplied buffer of specified length.
- *
- * @param   tx_pdu          TX_PDU structure to encode to binary format.
- * @param   buffer          Byte buffer where encoded TX_PDU will be stored.
- * @param   buffer_size     Size of supplied buffer.
- * @param   enc_len         Will contain the number of encoded bytes, on success.
- *
- * @return  encoded bytes   On success, the number of encoded bytes is returned.
- *          -1              On failure.
  */
 int pdu_encode(TX_PDU* tx_pdu, char* buffer, int buffer_size, int *enc_len)
 {
@@ -103,13 +95,6 @@ int pdu_encode(TX_PDU* tx_pdu, char* buffer, int buffer_size, int *enc_len)
 
 /**
  * Decode encoded buffer to supplied TX_PDU structure;
- *
- * @param   tx_pdu          TX_PDU structure with decoded data.
- * @param   buffer          Byte buffer where encoded TX_PDU is be stored.
- * @param   buffer_size     Size of supplied buffer.
- *
- * @return  Consumed bytes  On success, the number of bytes consumed from buffer is returned.
- *          -1              On failure.
  */
 int pdu_decode(TX_PDU* tx_pdu, char* buffer, int buffer_size)
 {
